e7a72e78e83f36831845289244aa275426f69936
Ernesto Level <Ernst@MacBook.local>
2009-11-04 19:37:06 -0430

    =)

--- image_evolve.cpp
+++ image_evolve.cpp
@@ -3,17 +3,18 @@
 using namespace std;
 
 int main() {
-  candidate_t *fittest;
-  population_t *pop;
-  pop = new population_t();
-  
-  for (int count = 0; count <= 500; count++) {
-    printf("Generando próxima población... [%d]\n", count);
-    pop->next_generation();
-    fittest = pop->get_fittest();
-    printf("Fitness: %f %%\n", (double) fittest->fitness * 100);
-    
-    if (count % 5 == 0)
-      fittest->write();
-  }
+	candidate_t *fittest;
+	population_t *pop;
+	pop = new population_t();
+	
+	for (int count = 0; count <= 500; count++) {
+		printf("Generando pr—xima poblaci—n... [%d]\n", count);
+		pop->next_generation();
+		
+		fittest = pop->get_fittest();
+		printf("  Fitness: %f %%\n", (double) fittest->fitness * 100);
+		
+		if (count % 5 == 0)
+			fittest->write();
+	}
 }
